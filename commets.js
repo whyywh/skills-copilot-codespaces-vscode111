3f15aaf26fec83f787ea46fac3d3b9bd18c5ba88
ywh <131283607+whyywh@users.noreply.github.com>
2025-08-29 13:27:27 +0000

    Copilot third commit

---
+++ commets.js
@@ -0,0 +1,12 @@
+// Create web server
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+
